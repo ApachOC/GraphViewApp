fb581941cc670c36abca10250b22f02e6bd1276a
ApachOC <wernerv@students.zcu.cz>
2021-01-12 19:47:33 +0100

    Update MongoDB Docker init script.

--- initialize-mongodb.js
+++ initialize-mongodb.js
@@ -1,25 +1,13 @@
-// db.createUser(
-//     {
-//         user: "root",
-//         pwd: "toor",
-//         roles: [
-//             {
-//                 role: "readWrite",
-//                 db: "graph-app"
-//             }
-//         ]
-//     }
-// );
 db.users.drop();
 db.users.insert({
-    "username" : "user",
+    "_id" : "user",
     "password" : "$2a$10$C/yCdT49HGkiy7.A4T3B7OkIe..yXWI/qJYQcU57GBfOgD04h6w/G",
     "email" : "user@example.com",
     "displayName" : "Default User",
     "roles" : ["user"]
 });
 db.users.insert({
-    "username" : "admin",
+    "_id" : "admin",
     "password" : "$2a$10$8b35le8aFgL.6X5C.d5Tu.ZnmwmjGfmMms6wm68G2.I2BdaGDgRZm",
     "email" : "admin@example.com",
     "displayName" : "Administrator",
